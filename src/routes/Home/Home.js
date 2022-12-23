371c487bff803e7996a210705f95f60080c23bfc
Manuel Juarez <manusho.asdasd.1@mail.com>
2022-12-23 00:26:13 -0300

    commit dos

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -3,8 +3,7 @@ import ItemListContainer from '../ItemListContainer/ItemListContainer';
 const Home = () => {
     return (
         <div>
-            <h1>Bienvenido a mi ecommerce!</h1>
-            <ItemListContainer />
+            
         </div>
     );
 };
